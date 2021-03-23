05b36202979c5833f989111fcfd3636783b9cfd5
tothzalan <toth.zalan03@gmail.com>
2021-03-23 15:17:33 +0100

    Local storage support The application saves the last loaded link to the local storage.

--- rssReader/Program.cs
+++ rssReader/Program.cs
@@ -18,6 +18,7 @@ namespace rssReader
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<LocalStorageService>();
 
             await builder.Build().RunAsync();
         }
